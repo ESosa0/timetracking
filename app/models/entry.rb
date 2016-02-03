c5a01b11a9ef7fbdafa815d1aebc9fdabcddf938
ESosa0 <ericasosa0@gmail.com>
2016-02-03 10:28:51 +0100

    added validations for Entry class

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,7 @@
 class Entry < ActiveRecord::Base
+  validates :hours, presence: true, numericality: true
+  validates :minutes, presence: true, numericality: true
+  validates :date, presence: true, 
+  validates :project, presence: true,  
   belongs_to :project
 end
