7d452561310b3273396af04af0cb1375cccfd33a
ESosa0 <ericasosa0@gmail.com>
2016-02-04 16:10:13 +0100

    added people controller, person model, and participation model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,19 @@
 class Project < ActiveRecord::Base
+
   has_many :entries
+  has_many :participations
+  has_many :people, through: :participations
+
   validates :name, presence: true, uniqueness: true, length: {maximum: 30}, format: {without: /[^[:alnum:]]/}
+  
   def self.iron_find(id)
     where(id: id).first
   end
+  
   def clean_old
     date = Time.now.midnight - 1.week
     projects = Project.where("created_at < ?", date)
     projects.destroy_all
+  
   end
 end
